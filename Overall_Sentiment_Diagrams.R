579762720b86d0ff443ee6c593e17e33f047f4e6
Samantha Su <114623857+samanthasu12@users.noreply.github.com>
2023-01-25 04:29:34 -0800

    Add files via upload

--- Overall_Sentiment_Diagrams.R
+++ Overall_Sentiment_Diagrams.R
@@ -1,12 +1,29 @@
-# Importing the required libraries
+install.packages('ggplot')
+library(ggplot)
 library(ggplot2)
 library(dplyr)
 
-count_3 <- c(round((449/(449+393+262))*100,2),round((393/(449+393+262))*100,2),round((262/(449+393+262))*100,2))
-# sum(count_3) # 100
+#reddit
+count <- c(round((449/(449+393+262))*100,2),round((393/(449+393+262))*100,2),round((262/(449+393+262))*100,2))
 
-pie<-pie(round(count_3,2), labels = paste0(count_3, "%"),col=c('palegreen2','brown1','lightgoldenrod1'), main="Reddit")
+pie<-pie(round(count,2), labels = paste0(count, "%"),col=c('palegreen2','brown1','lightgoldenrod1'), main="Reddit")
 
 legend("topleft", legend = c("Positive", "Negative", "Neutral"),
        fill =  c("palegreen2", "brown1", "lightgoldenrod1"))
 
+
+#discord
+count_1 <- c(round((37/(87+72+37))*100,2),round((72/(87+72+37))*100,2),round((87/(87+72+37))*100,2))
+
+pie<-pie(round(count_1,2), labels = paste0(count_1, "%"),col=c('palegreen2','brown1','lightgoldenrod1'), main="Discord")
+
+legend("topleft", legend = c("Positive", "Negative", "Neutral"),
+       fill =  c("palegreen2", "brown1", "lightgoldenrod1"))
+
+#twitter
+count_2<-count_1 <- c(round((291/(381+291+95))*100,2),round((95/(381+291+95))*100,2),round((381/(381+291+95))*100,2))
+
+pie<-pie(round(count_1,2), labels = paste0(count_1, "%"),col=c('palegreen2','brown1','lightgoldenrod1'), main="Twitter")
+
+legend("topleft", legend = c("Positive", "Negative", "Neutral"),
+       fill =  c("palegreen2", "brown1", "lightgoldenrod1"))                               # Print example data
